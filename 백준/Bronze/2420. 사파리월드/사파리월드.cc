119b762606190e405005d5ad9cd012d21f9ab193
나영환 <89558087+NYH0227@users.noreply.github.com>
2022-04-24 00:58:11 +0900

    [Bronze IV] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    long long a,b;
+    long long result = 0;
+    
+    cin >> a >> b;
+    result = abs(a-b);
+    cout<<result<<endl;
+    return 0;
+}
+
