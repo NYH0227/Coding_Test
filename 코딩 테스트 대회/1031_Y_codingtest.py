70e4086a9262f34d7f3ef9df9f296251c867c227
나영환 <89558087+NYH0227@users.noreply.github.com>
2022-04-23 23:44:41 +0900

    Update 1031_Y_codingtest.py

--- 코딩 테스트 대회/1031_Y_codingtest.py
+++ 코딩 테스트 대회/1031_Y_codingtest.py
@@ -1,3 +1,12 @@
+# 2021 연세대학교 프로그래밍 경진대회 Open Contest
+# 2021.10.31
+# 
+# 문제.1 입력받은 문자열에 'd2' 라는 문자가 포함하는 지 확인하는 문제
+# 문제.2 그리드 문제
+# 문제.3 팰린드롬 문제에서 접두사나 접미사도 팰린드롬인지 재귀로 푸는 문제
+
+
+#문제 1
 import sys
 
 result = 'unrated'
@@ -26,6 +35,7 @@ for e in A:
     
 print(result)
 
+# 문제 2 당시에 틀렸던 코드
 from collections import deque
 import sys
 
@@ -39,6 +49,20 @@ for i in set(A):
 
 print(result)
 
+
+# 문제.2 나중에 다시 푼 코드
+n = int(input()) 
+l = [0] * 1000001 
+for i in list(map(int, input().split())): 
+    l[i] += 1 
+c = 0 
+for i in list(map(int, input().split())): 
+    if l[i] >= 1: l[i] -= 1 
+    else: c += 1
+print(c)
+
+
+# 문제.3 
 import sys
 
 def aka(s):
@@ -54,40 +78,31 @@ def aka(s):
 s = sys.stdin.readline().strip()
 print("AKARAKA") if aka(s) == 1 else print('IPSELENTI')
 
+
+
+
+#
 import sys
-from collections import deque
 
-def BN(a,b):
-    print(list_m[a])
-    print()
+N,G,K = map(int, sys.stdin.readline().split())
+arr = []
+day = 0
 
-def BP(a,b):
-    pass
+for i in range(N):
+    n,g,k = map(int, sys.stdin.readline().split())
+    arr.append([n,g,k])
 
-def CP(a):
-    pass
+while(True):
+    deg = 0
+    for i in arr:
+        deg += i[0]*max(1,day - i[1])
+    arr.sort(key=lambda x:x[0]*max(1,day - x[1]))
 
-def CN(a):
-    pass
+    if K == 0 and G <= deg :
+        print(day-1)
+        break
 
-n,m = map(int,sys.stdin.readline().split())
-list_m = list(map(int, sys.stdin.readline().split()))
-
-for i in range(n):
-    func = list(map(str, sys.stdin.readline().split()))
-    
-    if func[0] == 'BN':
-        BN(func[1],func[2])
-    elif func[0] =='BP':
-        BP(func[1],func[2])
-    elif func[0] =='CP':
-        CP(func[1])
-    elif func[0] =='CN':
-        BN(func[1])
-    
-
-
-
-
-
-
+    day += 1
+    if G<= deg:
+        arr.pop(0)
+        K -= 1
