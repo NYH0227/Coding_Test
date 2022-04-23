fccd76ba5e81c80dddacde738fc76306511d3db3
나영환 <89558087+NYH0227@users.noreply.github.com>
2022-04-24 00:51:45 +0900

    [Silver III] Title: 스택 수열, Time: 4220 ms, Memory: 32444 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,21 @@
+n = int(input())
+s = []
+op = []
+count = 1
+temp = True
+for i in range(n):
+    num = int(input())
+    while count <= num:
+        s.append(count)
+        op.append('+')
+        count += 1
+    if s[-1] == num:
+        s.pop()
+        op.append('-')
+    else:
+        temp = False
+if temp == False:
+    print('NO')
+else:
+    for i in op:
+        print(i)
