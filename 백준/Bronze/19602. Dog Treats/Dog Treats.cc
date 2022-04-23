e13dc67d8363229d84c208ad1edb0c5b7b4eba5c
나영환 <89558087+NYH0227@users.noreply.github.com>
2022-04-24 02:16:59 +0900

    [Bronze IV] Title: Dog Treats, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/19602. Dog Treats/Dog Treats.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int S,M,L;
+    cin>>S;
+    cin>>M;
+    cin>>L;
+
+    if(1*S+2*M+3*L >= 10){
+        cout<<"happy";
+    }else{
+        cout<<"sad";
+    }
+
+}
