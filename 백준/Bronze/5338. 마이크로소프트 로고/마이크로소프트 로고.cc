f0e732f1be2b3a3174a2585442a0cf9624929466
나영환 <89558087+NYH0227@users.noreply.github.com>
2022-04-24 01:24:49 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cc
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "       _.-;;-._\n" << "'-..-'|   ||   |\n" << "'-..-'|_.-;;-._|\n" << "'-..-'|   ||   |\n" << "'-..-'|_.-''-._|";
+	return 0;
+}
