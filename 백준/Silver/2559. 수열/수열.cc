487eef69b65c2b9c26fd464338fd97f5f614919e
나영환 <89558087+NYH0227@users.noreply.github.com>
2022-04-26 16:46:33 +0900

    [Silver III] Title: 수열, Time: 24 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int N,K;
+    cin>> N>>K;
+    
+    int arr[N];
+    for(int i = 0 ; i<N; i++){
+        cin>>arr[i];
+    }
+    
+    int result[N-K+1];
+    int sum = 0;
+    int max1 = -100*(N+4);
+    for(int i = 0 ; i<N; i++){
+        if (i<K-1) sum+=arr[i];
+        else{
+            sum=sum+arr[i]-arr[i-K];
+            max1 = max(max1,sum);
+        }
+    }
+    cout<<max1<<endl;
+}
