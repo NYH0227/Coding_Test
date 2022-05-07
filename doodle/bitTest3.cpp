d2bb30c23be0da4feb9b62afe1dbb622cbabc31c
나영환 <89558087+NYH0227@users.noreply.github.com>
2022-05-07 17:26:03 +0900

    Update bitTest3.cpp

--- doodle/bitTest3.cpp
+++ doodle/bitTest3.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
 
+using namespace std;
+
+int main(){
+    
+    char a = 'a';
+    char b = 'b';
+    
+    cout<<a<<" : "<<b<<"\n";
+    a ^= b;
+    b ^= a;
+    a ^= b;
+    cout<<a<<" : "<<b<<"\n";
+   
+}
