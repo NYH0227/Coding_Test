e5427ecc15aaf16d078cb23a0ac9a616f49d7d3d
나영환 <89558087+NYH0227@users.noreply.github.com>
2022-05-11 21:24:26 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 8 ms, Memory: 9832 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+long long dp[1000004];
+int T,n;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+    
+    for (int i=4; i<1000004; i++)
+        dp[i] = (dp[i-3]+dp[i-2]+dp[i-1])%1000000009;
+    
+    cin>>T;
+    while(T--){
+        cin>>n;
+        cout<<dp[n]<<"\n";
+    }
+    
+}
