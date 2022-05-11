e143ea3ead2ab9b35cb310291ebb87954945f8c0
나영환 <89558087+NYH0227@users.noreply.github.com>
2022-05-11 21:33:15 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+long long dp[1004];
+int n;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    for (int i=3; i<1004; i++)
+        dp[i] = (dp[i-2]+dp[i-1]) % 10007;
+    
+    cin>>n;
+    cout<<dp[n]<<"\n";
+    
+}
